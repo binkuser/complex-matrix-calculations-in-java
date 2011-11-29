f71d72b721aa537deb91cfa20f40aeb19c16b9f6
binkhorst@gmail.com <binkhorst@gmail.com@f572bc7f-4077-0edd-1ef9-4cea3775c71a>
2011-11-29 20:50:22 +0000

    first release

---
+++ probeersels/src/testjes/CMatrixfromFile.java
@@ -0,0 +1,91 @@
+package testjes;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import flanagan.complex.Complex;
+import flanagan.complex.ComplexMatrix;
+
+
+public class CMatrixfromFile {
+	
+	private String[][] matrix;
+	private	ComplexMatrix cmatrix;
+	private int cmcollength;
+	private int cmrowlength;
+	
+	public	CMatrixfromFile (String file) {
+		//build a string with the contents of a file and determine te number of future columns
+		BufferedReader inFile;
+		String data = "";
+		String line;
+
+		try {
+			cmcollength = 1;
+			int cl = 1;
+			inFile = new BufferedReader(new FileReader(file));
+			while ((line = inFile.readLine()) != null) {
+				data = data + line + "\n";
+				cl = line.split(" ").length;
+				if(cl > cmcollength) {
+				cmcollength = cl;
+				}
+			 }
+			
+		inFile.close();
+		
+		//build a matrix of string elements from the original string 
+		String[] rows = data.split("\n");	    
+		cmrowlength = rows.length;
+		//cmcollength = rows[0].split(" ").length;
+		matrix = new String[cmrowlength][];
+			    
+		//build a ComplexMatrix from the matrix of string elements
+		cmatrix = new ComplexMatrix(cmrowlength, cmcollength); 	    
+		int r = 0;
+		for (String row : rows) {   	
+			matrix[r++] = row.split(" ");
+			//convert array elements to complex values and update the complex matrix with these
+			int x = 0;
+			    for(String element : matrix[r-1]) {
+			    cmatrix.setElement(r-1, x++, Complex.parseComplex(element));
+			        }
+			        
+			    }	
+		}
+		catch (IOException e) {
+			 JOptionPane.showMessageDialog(null, "File error: " + e.toString());
+		}
+		
+	}
+	
+public ComplexMatrix returnComplexMatrix() {
+	return cmatrix;
+}
+
+public String[][] returnInputMatrix() {
+	return matrix;
+}
+
+public void printInputMatrix() {
+	System.out.println(Arrays.deepToString(matrix));
+}
+
+public int getCols() {	
+return cmcollength ;
+}
+
+public int getRows() {	
+	return cmrowlength ;
+	}
+
+public void printComplexMatrix() {
+	System.out.println(Arrays.deepToString(cmatrix.getArray()));
+}
+
+
+	}
---
+++ probeersels/src/testjes/ComplexMatrixEnhanced.java
@@ -0,0 +1,85 @@
+package testjes;
+
+import java.util.Arrays;
+
+import flanagan.complex.Complex;
+import flanagan.complex.ComplexMatrix;
+
+/**
+ * @author Gérard Binkhorst
+ * @version 0.1
+ * @since 2011-11-25
+ */
+public class ComplexMatrixEnhanced extends ComplexMatrix {
+
+	/**
+	 * A {@link ComplexMatrix} which may be filled with the contents of a local text file containing complex elements.
+	 * @param file {@link String}
+	 */
+	public ComplexMatrixEnhanced(String file) {	
+		super(cmat(file).getRows(), cmat(file).getCols());
+		this.setTwoDarray(cmat(file).returnComplexMatrix().getArray());		
+	}
+
+	/**
+	 * Constructor of the upper class which can be applied when an empty ComplexMatrixEnhanced should be created and no local text file is specified.
+	 * @param col {@link integer} number of columns
+	 * @param row {@link integer} number of rows
+	 */
+	public ComplexMatrixEnhanced(int col, int row) {	
+		super(col, row);		
+	}
+	
+	private static CMatrixfromFile cmat(String intfile) {
+		return new CMatrixfromFile(intfile);
+	}
+
+	/**
+	 * Prints the contents of a {@link ComnplexMatrixEnhanced} in a readable format to the console.
+	 */
+	public void printComplexMatrixEnhanced() {
+		System.out.println(Arrays.deepToString(this.getArray()));
+	}
+	
+	/**
+	 * Prints the contents of a {@link ComnplexMatrix} in a readable format to the console.
+	 * @param z
+	 */
+	public static void printComplexMatrix(ComplexMatrix z) {
+		System.out.println(Arrays.deepToString(z.getArray()));
+	}
+	
+	public int getMaxColnumber() {
+		int  c= this.getNcol();
+		return c-1;
+	}
+	
+	public int getMaxRownumber() {
+		int  c= this.getNrow();
+		return c-1;
+	}
+	
+	public Complex[] getColumn(int colnum) {		
+		int z=this.getMaxRownumber()+1;
+		Complex[] m = new Complex[z];	
+		for (int l= 0; l<z; l++){
+		m[l] = this.getElementCopy(l, colnum);
+	}
+		return m;
+	}
+	public Complex[] getSolutionVector() {
+		Complex[] x = this.getSubMatrix(0,1,this.getMaxRownumber(),this.getMaxColnumber()).solveLinearSet(this.getColumn(0));
+		return x;
+	}
+	
+	public static void printComplexVector(Complex[] v) {
+		System.out.println(Arrays.deepToString(v));
+	}
+	
+	public static void main(String[] args) {
+		ComplexMatrixEnhanced mm = new ComplexMatrixEnhanced("input.txt");
+		mm.printComplexMatrixEnhanced();
+		ComplexMatrixEnhanced.printComplexVector(mm.getSolutionVector());
+		
+}
+}
